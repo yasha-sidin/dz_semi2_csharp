4eb8eea2ada01db7adc33deac318da4e39c3419d
Yasha <yashasidin333@yandex.ru>
2023-03-01 15:56:22 +0300

    Add solve of Task_15

---
+++ Task_15/Program.cs
@@ -0,0 +1,19 @@
+﻿// 
+Console.Write("Введите число от 1 до 7: ");
+int day = int.Parse(Console.ReadLine());
+
+if (day < 8 && day > 0)
+{
+    if (day == 6 && day == 7)
+    {
+        Console.Write("Выходной");
+    }
+    else
+    {
+        Console.Write("Не выходной");
+    }
+}
+else
+{
+    Console.Write("Не день недели");
+}
